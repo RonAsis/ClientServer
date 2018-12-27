380891e0460e8fb0c3aac9defc3849dbcf20ca48
ronasis <ronasi@post.bgu.ac.il>
2018-12-27 21:31:45 +0200

    ghghf

--- Client/src/ClientMain.cpp
+++ Client/src/ClientMain.cpp
@@ -3,7 +3,7 @@
 #include "../include/Client.h"
 #include <thread>
 //#include <boost/asio.hpp>
-int main (int argc, char *argv[]) {
+int main (int argc, char **argv) {
     if (argc < 3) {
         std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
         return -1;
